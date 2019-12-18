15b51453cf7f68511c91a8af978c8cb392c4a91c
Aysel Bayram <ayselbyram83@gmail.com>
2019-12-18 12:50:51 -0500

    add gittest.java

--- src/test/java/login.java
+++ src/test/java/login.java
@@ -1,12 +1,10 @@
 public class login {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
 //this line is written from intelliJ
-=======
 
         
         
-        //ADDED FROM GITHUB
->>>>>>> refs/remotes/origin/master
+
     }
 }
